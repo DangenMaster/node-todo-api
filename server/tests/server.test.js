a1bb9a7e451be769af7e393bc5f15e6092eb5dd6
DangenMaster <dangenmaster02@gmail.com>
2018-10-17 12:18:53 +0530

    Add GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -9,7 +9,23 @@ beforeEach((done) => {
 });
 
 describe('POST /todos', () => {
-    it('should save a new todo', (done) => {
+    it('should not save a new todo with invalid data', (done) => {
+        st(app)
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should save a new todo with valid data', (done) => {
         var text = 'Test todo route';
 
         st(app)
@@ -26,21 +42,6 @@ describe('POST /todos', () => {
                 Todo.find().then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
-                    done();
-                }).catch((e) => done(e));
-            });
-    });
-    it('should not save a new todo with invalid data', (done) => {
-        st(app)
-            .post('/todos')
-            .send({})
-            .expect(400)
-            .end((err, res) => {
-                if (err) {
-                    return done(err);
-                }
-                Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
                     done();
                 }).catch((e) => done(e));
             });
